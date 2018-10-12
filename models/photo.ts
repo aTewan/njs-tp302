ffb7a9bba1212caa2a5a1b3cfbb79e53313612ee
aTewan <tewan33@gmail.com>
2018-10-12 15:01:29 +0200

    Ajout fonction findById

--- models/photo.ts
+++ models/photo.ts
@@ -21,11 +21,17 @@ export class Photo implements PhotoSchema {
   }
 
   static async create(photo : Photo) {
-    await api.post<Photo>('users/',{photo})
+    await api.post<Photo>('photos/',{photo})
   }
 
   static async deleteById(photoId: number) {
-    await api.delete(`albums/${photoId}`)
+    await api.delete(`photos/${photoId}`)
+  }
+
+  static async findById(photoId: number):Promise<Photo> {
+    const { data } = await api.get<PhotoSchema>(`photos/${photoId}`)
+    const photo = new Photo(data);
+    return photo
   }
 }
 
